2b16e3ca5059d97aa7437075fc24066615f9f224
SantiagoR0214 <santiagoromero@uninorte.edu.co>
2024-11-18 23:54:52 -0500

    Crear seguimiento and fix errors

---
+++ src/hooks/useCreateFollow.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createFollow } from "../services/followServices";
+
+type ClientContact = {
+  firstname: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+};
+
+export type FollowType = {
+  opportunity_id: number;
+  contact_type: string;
+  contact_date: string; 
+  client_contact: ClientContact;
+  executive: string;
+  followup_description: string;
+  id?: number;
+};
+
+export const useCreateFollow = () => {
+  const queryFollow = useQueryClient();
+
+  return useMutation({
+    mutationFn: (follow: Omit<FollowType, 'id'>) => createFollow(follow),
+    onSuccess: () => {
+      queryFollow.invalidateQueries({ queryKey: ["follow"] });
+    },
+  });
+};
